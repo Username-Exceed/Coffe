face516c72ffb059f1eed33b8f511002b2c1e6f0
Username-Exceed <63782993+Username-Exceed@users.noreply.github.com>
2020-04-16 07:17:27 -0400

    Update menu-effects.js

--- jquery/menu-effects.js
+++ jquery/menu-effects.js
@@ -8,7 +8,7 @@ function main(){
 
         if(contador == 1){
             $('.nav-content').animate({
-                bottom:'-267px' 
+                bottom:'-265px' 
             });
             contador = 0;
         } else {
