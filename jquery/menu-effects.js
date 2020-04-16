019cebfc3486eba007541686cd7c22fd27359799
Username-Exceed <manprines@outlook.com>
2020-04-16 06:30:51 -0400

    primer commit

---
+++ jquery/menu-effects.js
@@ -0,0 +1,22 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+    $('.btn-bar').click(function(){
+        //$('.nav-content').toggle();
+
+        if(contador == 1){
+            $('.nav-content').animate({
+                bottom:'-265px'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('.nav-content').animate({
+                bottom:'423px'
+            })
+        }
+
+    });
+};
